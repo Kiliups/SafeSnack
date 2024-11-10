37186f0b644d8f09c552332fee7b589fbfec33cb
Kiliups <kilian.mayer@st.oth-regensburg.de>
2024-11-10 13:19:50 +0100

    changed frontend to svelte 5

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import '../app.css';
+
+    let {children} = $props();
+</script>
+
+<section class="w-screen h-screen">
+    {@render children()}
+</section>
+
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="flex flex-col justify-center items-center w-full h-full">Hello World!</h1>
