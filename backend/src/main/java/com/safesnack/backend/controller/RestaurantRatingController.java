10b1433c7e21ef40308f0996103dfcd21d1d3a4e
Kiliups <kilian.mayer@st.oth-regensburg.de>
2024-11-18 17:21:47 +0100

    generation of frontend api classes

--- backend/src/main/java/com/safesnack/backend/controller/RestaurantRatingController.java
+++ backend/src/main/java/com/safesnack/backend/controller/RestaurantRatingController.java
@@ -2,26 +2,27 @@ package com.safesnack.backend.controller;
 
 import com.safesnack.backend.model.RestaurantRating;
 import com.safesnack.backend.service.RestaurantRatingService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.logging.Logger;
 
-@RestController("/restaurantRating")
-@RequiredArgsConstructor
+@RestController
+@AllArgsConstructor
 public class RestaurantRatingController {
 
     private final RestaurantRatingService service;
 
-    @PostMapping
-    public void createRestaurantRating(@RequestBody RestaurantRating restaurantRating) {
-
-    }
-    @GetMapping
+    @GetMapping("/restaurantRating")
     public List<RestaurantRating> getRestaurantRating() {
-        return (List<RestaurantRating>) service.findAll();
+        Logger logger = Logger.getLogger(RestaurantRatingController.class.getName());
+        logger.info("RestaurantRatingController.getRestaurantRating()");
+        return service.findAll();
     }
 }
---
+++ frontend/src/api-src/models/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Restaurant';
+export * from './RestaurantRating';
+export * from './User';
